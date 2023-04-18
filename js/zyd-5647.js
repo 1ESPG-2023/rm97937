81b179835f0e2656af844fead9df6d36323efddf
Petrus369 <rm97937@fiap.com.br>
2023-04-18 10:58:46 -0300

    Spread + html collections + map

--- js/zyd-5647.js
+++ js/zyd-5647.js
@@ -1,5 +1,3 @@
-const liElement = document.getElementsByTagName("li");
-
 // for (let index = 0; index < liElement.length; index++) {
 //     console.log(liElement[index].textContent)
     
@@ -9,17 +7,36 @@ const liElement = document.getElementsByTagName("li");
 
 // console.log(liElement);
 
+// 
 //Declaração de Array:
-let num1 = [1, 2, 3, 4, 5];
-let num2 = [6, 7, 8, 9, 10];
-let num3 = [num1, num2];
+// let num1 = [1, 2, 3, 4, 5];
+// let num2 = [6, 7, 8, 9, 10];
+// let num3 = [...num1, ...num2];
 
-console.log(`Array 1: ${num1}`);
-console.log(`Array 2: ${num2}`);
-console.log(`Array 3: ${num3}`);
+// console.log(`Array 1: ${num1}`);
+// console.log(`Array 2: ${num2}`);
+// console.log(`Array 3: ${num3}`);
 
-//Listando o Array com técnica especial (forEach):
-num3.forEach((itemDoArray)=>{
-    console.log(`Array 3 sendo listado: ${itemDoArray}`)
+// //Listando o Array com técnica especial (forEach):
+// num3.forEach((itemDoArray)=>{
+//     console.log(`Array 3 sendo listado: ${itemDoArray}`)
+// })
+// console.log(`Array 3 sendo listado: ${num3}`);
+
+// //Listando o Array com MAP:
+// num3.map((numero, key)=>{
+//     console.log(`${(key+1)} - Itens do Array - ${numero}`);
+//     numero.forEach((digitos)=>{
+//         console.log(digitos);
+//     })
+// })
+
+//Convertendo HTMLCollection em Array e forma indireta:
+const liElementHTML = document.getElementsByTagName("li");
+const liElementArray = [...liElementHTML];
+
+liElementArray.map((item, key)=>{
+    if (item.textContent == "Item - 39"){
+        console.log(`${key+1} - ${item.textContent = "Localizado"}`);
+    }
 })
-console.log(`Array 3 sendo listado: ${num3}`);
